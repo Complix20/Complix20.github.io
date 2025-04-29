d9781fcf8ffe0cb22dd01854442cfbad7b1e1e4c
AlejandroC <alexcabero@hotmail.com>
2025-04-28 23:58:27 -0500

    cumple

--- js/script.js
+++ js/script.js
@@ -1,86 +1,11 @@
-"use strict";
+document.addEventListener("DOMContentLoaded", () => {
+  const mensajeBtn = document.querySelector(".btn--yes");
+  const mensajeBox = document.querySelector(".mensaje-box");
+  const mensajeText = document.querySelector(".mensaje-text");
 
-const titleElement = document.querySelector(".title");
-const buttonsContainer = document.querySelector(".buttons");
-const yesButton = document.querySelector(".btn--yes");
-const noButton = document.querySelector(".btn--no");
-const catImg = document.querySelector(".cat-img");
-
-const MAX_IMAGES = 5;
-let play = true;
-let noCount = 0;
-
-// Event Listeners
-yesButton.addEventListener("click", handleYesClick);
-
-noButton.addEventListener("mouseover", () => {
-  if (noCount >= MAX_IMAGES) {
-    const randomX = Math.random() * 200 - 100;
-    const randomY = Math.random() * 200 - 100;
-    noButton.style.transform = `translate(${randomX}px, ${randomY}px)`;
-  }
+  mensajeBtn.addEventListener("click", () => {
+    mensajeText.textContent = "¡Espero que tengas un día hermoso! Te lo mereces muchísimo, eres una gran persona y en realidad cada vez que te voy conociendo mas, me doy cuenta lo increible y bien que se siente estar a tu lado, una vez mas pasalo bello y espero te guste el pequeño detalle :D  ❤️🎉🎂";
+    mensajeBox.classList.remove("hidden");
+    confetti();
+  });
 });
-
-noButton.addEventListener("click", function () {
-  if (play) {
-    noCount++;
-    const imageIndex = Math.min(noCount, MAX_IMAGES);
-    changeImage(imageIndex);
-    resizeYesButton();
-    updateNoButtonText();
-    
-    if (noCount === MAX_IMAGES) {
-      play = false;
-      noButton.style.transform = "translate(0, 0)";
-    }
-  }
-});
-
-// Funciones
-function handleYesClick() {
-  // Mostrar mensaje y ocultar botones
-  titleElement.innerHTML = "Yayyy!! :3";
-  buttonsContainer.classList.add("hidden");
-
-  // Cambiar la imagen a la de "sí"
-  changeImage("yes");
-
-  // Lanzar confetti
-  triggerConfetti();
-}
-
-function resizeYesButton() {
-  const computedStyle = window.getComputedStyle(yesButton);
-  const newFontSize = parseFloat(computedStyle.fontSize) * 1.6;
-  yesButton.style.fontSize = `${newFontSize}px`;
-}
-
-function generateMessage(noCount) {
-  const messages = [
-    "No",
-    "Estas segura?",
-    "Porfis :c",
-    "Voy a llorar :(",
-    "Me voy a morir",
-    "Enserio moriré....",
-  ];
-  return messages[Math.min(noCount, messages.length - 1)];
-}
-
-function changeImage(image) {
-  catImg.src = `images/cat-${image}.jpg`;
-}
-
-function updateNoButtonText() {
-  noButton.textContent = generateMessage(noCount);
-}
-
-function triggerConfetti() {
-  if (window.confetti) {
-    confetti({
-      particleCount: 100,
-      spread: 70,
-      origin: { y: 0.6 },
-    });
-  }
-}
