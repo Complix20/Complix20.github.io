c59383da7d16dbe235585a0cc257fc3460ffa3ce
AlejandroC <alexcabero@hotmail.com>
2025-07-30 21:50:06 -0500

    aloha

--- js/script.js
+++ js/script.js
@@ -1,11 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const mensajeBtn = document.querySelector(".btn--yes");
+  const btnYes = document.querySelector(".btn--yes");
+  const btnNo = document.querySelector(".btn--no");
   const mensajeBox = document.querySelector(".mensaje-box");
   const mensajeText = document.querySelector(".mensaje-text");
 
-  mensajeBtn.addEventListener("click", () => {
-    mensajeText.textContent = "¡Espero que tengas un día hermoso! Te lo mereces muchísimo, eres una gran persona y en realidad cada vez que te voy conociendo mas, me doy cuenta lo increible y bien que se siente estar a tu lado, una vez mas pasalo bello y espero te guste el pequeño detalle :D  ❤️🎉🎂";
-    mensajeBox.classList.remove("hidden");
-    confetti();
+  btnYes.addEventListener("click", () => {
+  mensajeText.textContent = "Ahora todo es felicidad 😋🎉";
+  mensajeBox.classList.remove("hidden");
+  document.querySelector(".buttons").classList.add("hidden"); 
+  confetti();
+});
+
+
+  btnNo.addEventListener("mouseover", () => {
+    const container = document.querySelector(".container");
+    const maxX = container.offsetWidth - btnNo.offsetWidth;
+    const maxY = container.offsetHeight - btnNo.offsetHeight;
+
+    const randomX = Math.random() * maxX;
+    const randomY = Math.random() * maxY;
+
+    btnNo.style.position = "absolute";
+    btnNo.style.left = `${randomX}px`;
+    btnNo.style.top = `${randomY}px`;
   });
 });
