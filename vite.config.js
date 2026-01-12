3f2e0c6437f45c70578e46f6aa3dad0a0b1012b3
AlejandroC <alexcabero@hotmail.com>
2026-01-12 04:38:49 -0500

    hola

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Complix20.github.io/',
+  base: '/Complix20.github.io',
 })
